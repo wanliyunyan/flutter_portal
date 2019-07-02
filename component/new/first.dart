2cadd5b5a7ff9b130e3087472d4fb7e21c287253
26383604@qq.com <26383604@qq.com>
2019-07-02 16:43:33 +0800

    冒号

--- component/new/first.dart
+++ component/new/first.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 class FirstA extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return new Container(
-      child:Text("lalalal")
-    )
+  FirstA(){
+    @override
+    Widget build(BuildContext context) {
+      return new Container(
+          child:Text("lalalal")
+      );
+    }
   }
+
 }
