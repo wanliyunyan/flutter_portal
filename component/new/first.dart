0838424c7cd975dceb368361c50c32b26a5e4c81
26383604@qq.com <26383604@qq.com>
2019-07-02 16:32:14 +0800

    test

--- component/new/first.dart
+++ component/new/first.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-class First extends StatelessWidget{
+class FirstA extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return new Container(
