c5682d49d85e951ab491b5ebb21ecd183d9e1450
26383604@qq.com <26383604@qq.com>
2019-07-03 10:16:26 +0800

    路由

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,14 @@ import 'package:flutter_portal/component/new/first.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application. aa a a
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
         title: "test",
         home: Scaffold(
             appBar: AppBar(title: Center(child: Text("主页"))),
-            body: Center(child:FirstA(name:"我传递eee进来了!"))
+            body: Center(child:FirstA(name:"点你点你点你"))
         )
     );
   }
