2b9c80e5d40898670695cd2e3788306e34b061e1
26383604@qq.com <26383604@qq.com>
2019-07-03 15:33:45 +0800

    带状态组件用法

--- lib/router/pageFour/index.dart
+++ lib/router/pageFour/index.dart
@@ -8,6 +8,7 @@ class FourWidget extends StatefulWidget{
 }
 
 class Four extends State<FourWidget>{
+  int index=1;
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -15,7 +16,20 @@ class Four extends State<FourWidget>{
         title: new Text('第四页'),
       ),
       body: new Center(
-        child: Icon(Icons.warning,size: 130.0,color: Colors.blue,),
+        child:Column(children: <Widget>[
+          Icon(Icons.warning,size: 130.0,color: Colors.blue,),
+          Text("$index 计数"),
+          MaterialButton(
+            color: Colors.blue,
+            textColor: Colors.white,
+            child: new Text('点我'),
+            onPressed: () {
+              setState(() {
+                index ++;
+              });
+            },
+          )
+        ],)
       ),
     );
   }
