023d03e23ba923b9419de398b4c5c2aa1c05a42f
fensexiaoniu <645587257@qq.com>
2019-07-04 11:43:54 +0800

    新闻

--- lib/router/bottomNav/nav.dart
+++ lib/router/bottomNav/nav.dart
@@ -55,7 +55,7 @@ class BottomNavigationWidgetState extends State<BottomNavigationWidget> {
                   Icons.business,
                 ),
                 title: new Text(
-                  '商业',
+                  '资讯',
                 )),
             BottomNavigationBarItem(
                 icon: Icon(
--- lib/router/pageFour/index.dart
+++ lib/router/pageFour/index.dart
@@ -8,15 +8,36 @@ class FourWidget extends StatefulWidget{
 }
 
 class Four extends State<FourWidget>{
+  final arr=List<Widget>.generate(5,(i){
+    return Card(
+      child: Image.network('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg'),
+
+    );
+    /*return Container(
+      height: 250.00,
+      width:  double.infinity,
+      margin: EdgeInsets.all(5.00),
+      decoration: new BoxDecoration(
+        image:DecorationImage(
+          image: NetworkImage('http://img5.mtime.cn/mt/2018/11/20/141608.71613590_135X190X4.jpg'),
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      child: Card(
+        margin: EdgeInsets.fromLTRB(0.0, 200.0, 0.0, 0.0),
+        child: Text('哈哈去哈哈哈哈',style:new TextStyle(color: Colors.lightGreen),),
+      ),
+    );*/
+  });
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('第四页'),
       ),
-      body: new Center(
-        child: Icon(Icons.warning,size: 130.0,color: Colors.blue,),
-      ),
+      body: Column(
+        children:arr
+      )
     );
   }
 }
