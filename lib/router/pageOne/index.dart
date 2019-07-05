7b5c5faf8f780462080c1fc55fad9d4f4ab0c01f
wanliyunyan <88527712pan@163.com>
2019-07-05 10:52:03 +0800

    包管理

--- lib/router/pageOne/index.dart
+++ lib/router/pageOne/index.dart
@@ -10,12 +10,31 @@ class OneWidget extends StatefulWidget{
 class One extends State<OneWidget>{
   @override
   Widget build(BuildContext context) {
+    //Scaffold是Material中主要的布局组件.
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('首页'),
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: new Text('Example title'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          ),
+        ],
       ),
+      //body占屏幕的大部分
       body: new Center(
-        child: Icon(Icons.home,size: 130.0,color: Colors.blue,),
+        child: new Text('我也来学习布局!'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: new Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
