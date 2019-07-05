4a671085d21ada2c717ab5054cfd82068a394ca7
26383604@qq.com <26383604@qq.com>
2019-07-05 15:15:08 +0800

    数据

--- lib/model/modelOne.dart
+++ lib/model/modelOne.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 class  Counter with ChangeNotifier {
   int value = 0;
+  bool _isFetching = false;
+  String _jsonResonse = "";
+  String _dataUrl = "https://reqres.in/api/users?per_page=20";
   void increment() {
     value++;
     notifyListeners();
   }
   int get getText => value;
+  bool get isFetching => _isFetching;
+  Future<void> fetchData() async {
+    _isFetching = true;
+    notifyListeners();
+    var response = await http.get(_dataUrl);
+    if (response.statusCode == 200) {
+      _jsonResonse = response.body;
+    }
+
+    _isFetching = false;
+    notifyListeners();
+  }
+  List<dynamic> getResponseJson() {
+    if (_jsonResonse.isNotEmpty) {
+      Map<String, dynamic> json = jsonDecode(_jsonResonse);
+      // print(json['data']['avatar']);
+      return json['data'];
+    }
+    return null;
+  }
 }
--- lib/router/pageTwo/index.dart
+++ lib/router/pageTwo/index.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../model/modelOne.dart';
-class TwoWidget extends StatefulWidget{
+
+class TwoWidget extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     return new Two();
   }
 }
 
-class Two extends State<TwoWidget>{
+class Two extends State<TwoWidget> {
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<Counter>(context);
@@ -17,17 +18,34 @@ class Two extends State<TwoWidget>{
         title: new Text('第二页'),
       ),
       body: new Center(
-        child: Column(
-          children: <Widget>[
-            Icon(Icons.label_outline,size: 130.0,color: Colors.blue,),
-              RaisedButton(
-                child: Text("点我"),
-                onPressed:appState.increment
-              ),
-            Text("${appState.getText} 次")
-          ],
-        )
-      ),
+          child: Column(
+        children: <Widget>[
+          RaisedButton(child: Text("点我"), onPressed: appState.fetchData),
+          Container(
+            height: appState.isFetching ? 50.0 : 400.0,
+            child: appState.isFetching
+                ? CircularProgressIndicator()
+                : appState.getResponseJson() != null
+                    ? ListView.builder(
+                        primary: false,
+                        shrinkWrap: true,
+                        itemCount: appState.getResponseJson().length,
+                        itemBuilder: (context, index) {
+                          return ListTile(
+                            leading: CircleAvatar(
+                              backgroundImage: NetworkImage(
+                                  appState.getResponseJson()[index]['avatar']),
+                            ),
+                            title: Text(
+                              appState.getResponseJson()[index]["first_name"],
+                            ),
+                          );
+                        },
+                      )
+                    : Text("点按钮请求数据"),
+          )
+        ],
+      )),
     );
   }
 }
